292f24ad9a515a9507dcf52801e8ead79b10b2fa
Navya <navyaparuchuri2616@gmail.com>
2025-09-22 09:36:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+import App from './App.jsx'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(React.createElement(App))import App from './App.jsx'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(React.createElement(App))
